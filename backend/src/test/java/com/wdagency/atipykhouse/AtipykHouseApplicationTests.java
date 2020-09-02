ec2caee0e1b4f10a4dcf76058d39486455ed7e0a
stefOuroboros <stephane.gallois89@gmail.com>
2020-09-02 09:54:05 +0200

    first commit, folders & back start

---
+++ backend/src/test/java/com/wdagency/atipykhouse/AtipykHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdagency.atipykhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtipykHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
