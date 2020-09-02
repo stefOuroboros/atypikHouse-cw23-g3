87b1bc9d177b9989efa4f9cfc89dea1b1bee02a5
stefOuroboros <stephane.gallois89@gmail.com>
2020-09-02 10:23:51 +0200

    build successfull

--- backend/src/main/java/com/wdagency/atipykhouse/model/House.java
+++ backend/src/main/java/com/wdagency/atipykhouse/model/House.java
@@ -1,5 +1,11 @@
 package com.wdagency.atipykhouse.model;
 
+import lombok.Data;
+
+@Data
 public class House {
 
+	
+	private long id;
+	private String name;
 }
--- backend/src/main/java/com/wdagency/atipykhouse/model/User.java
+++ backend/src/main/java/com/wdagency/atipykhouse/model/User.java
@@ -1,5 +1,10 @@
 package com.wdagency.atipykhouse.model;
 
+import lombok.Data;
+
+@Data
 public class User {
 
+	private long id;
+	private String name;
 }
