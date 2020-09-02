69566e9fd127785db6d2aa0bad1a99a448d98240
stefOuroboros <stephane.gallois89@gmail.com>
2020-09-02 11:36:41 +0200

    hello world controller + initiate Swagger

--- backend/src/main/java/com/wdagency/atipykhouse/controller/HouseController.java
+++ backend/src/main/java/com/wdagency/atipykhouse/controller/HouseController.java
@@ -1,5 +1,14 @@
 package com.wdagency.atipykhouse.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HouseController {
 
+	
+	@GetMapping(value="/test")
+	public String hello() {
+		return "hello world";
+	}
 }
