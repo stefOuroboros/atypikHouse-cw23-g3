6c25c3725f21fa3b1de4c02f1f78c99c5743c553
stefOuroboros <stephane.gallois89@gmail.com>
2020-09-02 12:10:33 +0200

    Swagger 2 restApi documentation available

--- backend/src/main/java/com/wdagency/atipykhouse/configuration/SwaggerConf.java
+++ backend/src/main/java/com/wdagency/atipykhouse/configuration/SwaggerConf.java
@@ -2,13 +2,17 @@ package com.wdagency.atipykhouse.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@Import(SpringDataRestConfiguration.class)
 @EnableSwagger2
 public class SwaggerConf {
 
@@ -18,6 +22,6 @@ public class SwaggerConf {
 	      .select()                                  
 	      .apis(RequestHandlerSelectors.any())              
 	      .paths(PathSelectors.any())                          
-	      .build();                                           
+	      .build();
 	}
 }
--- backend/src/main/java/com/wdagency/atipykhouse/controller/HouseController.java
+++ backend/src/main/java/com/wdagency/atipykhouse/controller/HouseController.java
@@ -1,9 +1,13 @@
 package com.wdagency.atipykhouse.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@CrossOrigin
+@RestController()
+@RequestMapping("/main")
 public class HouseController {
 
 	
